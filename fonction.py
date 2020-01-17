64b0bb7bc6061f2976cef51ff042f476b04f09a0
Flav52 <fr108211@MI105-15.iem>
2020-01-17 14:52:35 +0100

    verif py & sanitize json

--- fonction.py
+++ fonction.py
@@ -1,50 +1,54 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-user = "mongodb://nl435089:nl435089@mongo/?authSource=nl435089@authMechanisme=SCRAM-SHA-1"
+uri="mongodb://fr108211:fr108211@mongo/?authSource=fr108211&authMechanism=SCRAM-SHA-1"
 
-#On récupère la connexion à mongoDB
-c=MongoClient(user)
-db=c.nl435089 #id : nl435089 mdp : nl435089  Pour Flavien
-db.collection_name()
+#On recupère la connexion à mongoDB
+c=MongoClient(uri)
+db=c.fr108211
+db
 
-#Affiche tous les jeux disponibles sur le catalogue pour chaque catégorie :
-def jeuxCatégorie():
-    c = db.bibliotheque.aggregate([{"$unwind":"$categorie"},{"$sort":{"nom":1}},{"$group":{"_id":"$categorie","jeux":{"$push":{"nom":"$nom"}}}},{"$sort":{"_id":1}}])
+#Affiche tous les jeux disponibles sur le catalogue pour chaque categorie :
+def jeuxCategorie():
+    c = db.jeux.aggregate([{"$unwind":"$categorie"},{"$sort":{"nom":1}},{"$group":{"_id":"$categorie","jeux":{"$push":{"nom":"$nom"}}}},{"$sort":{"_id":1}}])
     for doc in c :
-        #Pour chaque catégorie on récupère les jeux et on les affiche
+        #Pour chaque categorie on recupère les jeux et on les affiche
         print("Categorie "+ doc["_id"]+" :")
         for j in doc["jeux"]:
             print("\t" + j["nom"])
         print()
 
-#Affichage des informations d'un utilisateur en ayant comme paramètre son pseudo
+#Affichage des avis publiés par un utilisateur
 def informationUser(pseudo):
-    c = db.CatalogueJeu.aggregate([{"$unwind" : "$avis"},{"$match" : {"avis.pseudo" : pseudo}},
-                                   {"$group" : {"_id" : "$avis"}}])
+    c = db.jeux.aggregate([{"$unwind" : "$avis"},{"$match" : {"avis.user.pseudo" : pseudo}},
+                            {"$group" : {"_id" : "$avis"}}])
     commentaire = False
     for doc in c:
         #Pour chacun des avis, on affiche les informations.
-        print("\t" + "Note : " + str(doc["_id"]["notation"]))
+        print("\t" + "Note : " + str(doc["_id"]["note"]))
         print("\t\t" + str(doc["_id"]["commentaire"]))
         print("")
-        com = True
+        commentaire = True
     if not commentaire :
         #Si aucun commentaire
-        print("L'utilisateur " + pseudo + " n'a jamais laissé de commentaire.")
+        print("L'utilisateur " + pseudo + " n'a jamais laisse de commentaire.")
 
-#Fait varier tous les prix en fonction d'un pourcentage donné, si pourcentage < 100 alors les prix baisseront sinon si pourcentage > 100 ils augmenteront
+#Fait varier tous les prix en fonction d'un pourcentage donné pour simuler des soldes
 def VariationPrixGlobal(pourcentage):
-    c = db.CatalogueJeu.find({"prix" : { "$exists" : True}});
+    c = db.jeux.find({"prix" : { "$exists" : True}});
     for doc in c:
         variation = ((pourcentage)*doc["prix"])/100
-        db.CatalogueJeu.update({"nom":doc["nom"]},{"$set":{"prix":variation}})
+        db.jeux.update({"nom":doc["nom"]},{"$set":{"prix":variation}})
         doc["prix"] = variation
-        print("Le prix du jeu " + doc["nom"] + " a varié de " + str(pourcentage) + " pourcent. Le prix est maintenant de : " + str(doc["prix"]) + "euro.")
+        print("Le prix du jeu " + doc["nom"] + " a varie de " + str(pourcentage) + " pourcent. Le prix est maintenant de : " + str(doc["prix"]) + "euro.")
 
 #Suppression des descriptions :
 def SuppressionDescription():
-    #On récupère tous les jeux, et on supprime l'attribut dans le document
-    db.CatalogueJeu.update_many({"description" : {"$exists" : True}},{"$unset" : {"description" : ""}})
-    print("Descriptions effacées")
+    #On recupère tous les jeux, et on supprime l'attribut dans le document
+    db.jeux.update_many({"description" : {"$exists" : True}},{"$unset" : {"description" : ""}})
+    print("Descriptions effacees")
 
 
+#jeuxCategorie()
+informationUser("LMAV")
+#VariationPrixGlobal(90)
+#SuppressionDescription()
